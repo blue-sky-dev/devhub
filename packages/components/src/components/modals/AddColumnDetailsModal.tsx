blue-sky-dev/devhub

commit 65d6a73876b8238c698d06b4fef4736a7f5ccc61

New column type: Notifications for a specific repository
Closes #44

packages/components/src/components/modals/AddColumnDetailsModal.tsx
@@ -30,6 +30,7 @@ import { Button } from '../common/Button'
 import { H2 } from '../common/H2'
 import { H3 } from '../common/H3'
 import { Spacer } from '../common/Spacer'
+import { SubHeader } from '../common/SubHeader'
 import {
   SpringAnimatedTextInput,
   SpringAnimatedTextInputProps,
@@ -77,12 +78,12 @@ const fields: FieldDetails[] = [
 export const AddColumnDetailsModal = React.memo(
   (props: AddColumnDetailsModalProps) => {
     const {
-      subscription,
-      showBackButton,
-      paramList,
-      name,
-      icon,
       defaultParams,
+      icon,
+      paramList,
+      showBackButton,
+      subscription,
+      title,
     } = props
 
     const didAutoFocusRef = useRef(false)
@@ -254,14 +255,10 @@ export const AddColumnDetailsModal = React.memo(
       [params, createTextInputChangeHandler, createTextInputSubmitHandler],
     )
 
-    const renderField = (field: string, index?: number) => {
-      const fieldDetails = fields.find(f => f.field === field)
+    const renderField = (fieldDetails: FieldDetails, index?: number) => {
+      if (!fieldDetails) return null
 
-      if (fieldDetails) {
-        return renderTextInput(fieldDetails, { autoFocus: index === 0 })
-      }
-
-      return null
+      return renderTextInput(fieldDetails, { autoFocus: index === 0 })
     }
 
     return (
@@ -271,32 +268,17 @@ export const AddColumnDetailsModal = React.memo(
         showBackButton={showBackButton}
         title="Add Column"
       >
-        <ScrollView
-          keyboardShouldPersistTaps="handled"
-          style={{ flex: 1, padding: contentPadding }}
-        >
-          <View
-            style={{
-              flexDirection: 'row',
-              alignItems: 'center',
-              alignContent: 'center',
-            }}
-          >
-            <ColumnHeaderItem
-              analyticsLabel={undefined}
-              iconName={icon}
-              noPadding
-            />
-            <Spacer width={contentPadding / 2} />
-            <H2>{name}</H2>
+        <ScrollView keyboardShouldPersistTaps="handled" style={{ flex: 1 }}>
+          <SubHeader iconName={icon} title={title} />
+
+          <View style={{ flex: 1, padding: contentPadding }}>
+            {(paramList
+              .map(p => fields.find(f => f.field === p))
+              .filter(Boolean) as FieldDetails[]).map(renderField)}
+            <Button analyticsLabel="add_column" onPress={handleCreateColumn}>
+              Add Column
+            </Button>
           </View>
-
-          <Spacer height={contentPadding} />
-
-          {paramList.map(renderField)}
-          <Button analyticsLabel="add_column" onPress={handleCreateColumn}>
-            Add Column
-          </Button>
 
           <Spacer height={contentPadding} />
         </ScrollView>

packages/components/src/components/modals/AddColumnModal.tsx
@@ -3,7 +3,12 @@ import React, { useEffect, useRef } from 'react'
 import { View } from 'react-native'
 import { useSpring } from 'react-spring/native-hooks'
 
-import { AddColumnDetailsPayload, constants } from '@devhub/core'
+import {
+  AddColumnDetailsPayload,
+  ColumnSubscription,
+  constants,
+  GitHubIcon,
+} from '@devhub/core'
 import { useCSSVariablesOrSpringAnimatedTheme } from '../../hooks/use-css-variables-or-spring--animated-theme'
 import { useHover } from '../../hooks/use-hover'
 import { useReduxAction } from '../../hooks/use-redux-action'
@@ -17,6 +22,7 @@ import { SpringAnimatedTouchableOpacity } from '../animated/spring/SpringAnimate
 import { ColumnHeaderItem } from '../columns/ColumnHeaderItem'
 import { ModalColumn } from '../columns/ModalColumn'
 import { separatorTickSize } from '../common/Separator'
+import { SubHeader } from '../common/SubHeader'
 import { useColumnWidth } from '../context/ColumnWidthContext'
 import { useTheme } from '../context/ThemeContext'
 
@@ -24,67 +30,122 @@ export interface AddColumnModalProps {
   showBackButton: boolean
 }
 
-const columnTypes: AddColumnDetailsPayload[] = [
+const columnTypes: Array<{
+  title: string
+  type: ColumnSubscription['type']
+  icon: GitHubIcon
+  items: Array<{
+    title: string
+    icon: GitHubIcon
+    payload: AddColumnDetailsPayload
+  }>
+}> = [
   {
-    name: 'Dashboard',
-    icon: 'home',
-    subscription: {
-      type: 'activity',
-      subtype: 'USER_RECEIVED_EVENTS',
-    },
-    paramList: ['username'],
-  },
-  {
-    name: 'User',
-    icon: 'person',
-    subscription: {
-      type: 'activity',
-      subtype: 'USER_EVENTS',
-    },
-    paramList: ['username'],
-  },
-  {
-    name: 'Notifications',
+    title: 'Notifications',
+    type: 'notifications',
     icon: 'bell',
-    subscription: {
-      type: 'notifications',
-      subtype: '',
-    },
-    paramList: ['all'],
+    items: [
+      {
+        title: 'Notifications',
+        icon: 'bell',
+        payload: {
+          icon: 'bell',
+          title: 'All notifications',
+          subscription: {
+            type: 'notifications',
+            subtype: undefined,
+          },
+          paramList: ['all'],
+        },
+      },
+      {
+        title: 'Repository',
+        icon: 'repo',
+        payload: {
+          icon: 'bell',
+          title: 'Repository notifications',
+          subscription: {
+            type: 'notifications',
+            subtype: 'REPO_NOTIFICATIONS',
+          },
+          paramList: ['all', 'owner', 'repo'],
+        },
+      },
+    ],
   },
   {
-    name: 'Organization',
-    icon: 'organization',
-    subscription: {
-      type: 'activity',
-      subtype: 'ORG_PUBLIC_EVENTS',
-    },
-    paramList: ['org'],
-  },
-  {
-    name: 'Repository',
-    icon: 'repo',
-    subscription: {
-      type: 'activity',
-      subtype: 'REPO_EVENTS',
-    },
-    paramList: ['owner', 'repo'],
+    title: 'Activity',
+    type: 'activity',
+    icon: 'rss',
+    items: [
+      {
+        title: 'Dashboard',
+        icon: 'home',
+        payload: {
+          icon: 'home',
+          title: 'User dashboard',
+          subscription: {
+            type: 'activity',
+            subtype: 'USER_RECEIVED_EVENTS',
+          },
+          paramList: ['username'],
+        },
+      },
+      {
+        title: 'User',
+        icon: 'person',
+        payload: {
+          icon: 'person',
+          title: 'User activity',
+          subscription: {
+            type: 'activity',
+            subtype: 'USER_EVENTS',
+          },
+          paramList: ['username'],
+        },
+      },
+      {
+        title: 'Organization',
+        icon: 'organization',
+        payload: {
+          icon: 'organization',
+          title: 'Organization activity',
+          subscription: {
+            type: 'activity',
+            subtype: 'ORG_PUBLIC_EVENTS',
+          },
+          paramList: ['org'],
+        },
+      },
+      {
+        title: 'Repository',
+        icon: 'repo',
+        payload: {
+          icon: 'repo',
+          title: 'Repository activity',
+          subscription: {
+            type: 'activity',
+            subtype: 'REPO_EVENTS',
+          },
+          paramList: ['owner', 'repo'],
+        },
+      },
+    ],
   },
 ]
-
-function keyExtractor(columnType: AddColumnDetailsPayload) {
-  return `add-column-button-${columnType.subscription.type}-${columnType
-    .subscription.subtype || ''}`
-}
 
 function AddColumnModalItem({
   availableWidth,
   disabled,
-  item,
+  icon,
+  payload,
+  title,
 }: {
   availableWidth: number
   disabled?: boolean
-  item: AddColumnDetailsPayload
+  icon: GitHubIcon
+  payload: AddColumnDetailsPayload
+  title: string
 }) {
   const initialTheme = useTheme(theme => {
     cacheRef.current.theme = theme
@@ -143,7 +204,7 @@ function AddColumnModalItem({
       onPress={() =>
         pushModal({
           name: 'ADD_COLUMN_DETAILS',
-          params: item,
+          params: payload,
         })
       }
       onPressIn={() => {
@@ -175,7 +236,8 @@ function AddColumnModalItem({
       >
         <ColumnHeaderItem
           analyticsLabel={undefined}
-          iconName={item.icon}
+          iconName={icon}
+          iconStyle={{ lineHeight: undefined }}
           noPadding
           size={24}
           style={{ alignSelf: 'center', marginBottom: contentPadding / 2 }}
@@ -184,9 +246,10 @@ function AddColumnModalItem({
         <SpringAnimatedText
           style={{
             color: springAnimatedTheme.foregroundColor,
+            textAlign: 'center',
           }}
         >
-          {item.name}
+          {title}
         </SpringAnimatedText>
       </View>
     </SpringAnimatedTouchableOpacity>
@@ -213,45 +276,53 @@ export function AddColumnModal(props: AddColumnModalProps) {
       showBackButton={showBackButton}
       title="Add Column"
     >
-      <View
-        style={{
-          flex: 1,
-          padding: outerSpacing,
-        }}
-      >
-        <View
-          style={{
-            flex: 1,
-            flexDirection: 'row',
-            flexWrap: 'wrap',
-            alignContent: 'flex-start',
-          }}
-        >
-          {columnTypes.map(item => (
-            <AddColumnModalItem
-              key={keyExtractor(item)}
-              availableWidth={availableWidth}
-              disabled={hasReachedColumnLimit}
-              item={item}
+      <View style={{ flex: 1 }}>
+        {columnTypes.map((group, groupIndex) => (
+          <View key={`add-column-header-group-${groupIndex}`}>
+            <SubHeader
+              analyticsLabel={undefined}
+              iconName={group.icon}
+              title={group.title}
             />
-          ))}
 
-          {!!hasReachedColumnLimit && (
-            <SpringAnimatedText
+            <View
               style={{
-                marginTop: contentPadding,
-                lineHeight: 20,
-                fontSize: 14,
-                color: springAnimatedTheme.foregroundColorMuted50,
-                textAlign: 'center',
+                flex: 1,
+                flexDirection: 'row',
+                flexWrap: 'wrap',
+                alignContent: 'flex-start',
+                padding: outerSpacing,
               }}
             >
-              {`You have reached the limit of ${
-                constants.COLUMNS_LIMIT
-              } columns. This is to maintain a healthy usage of the GitHub API.`}
-            </SpringAnimatedText>
-          )}
-        </View>
+              {group.items.map((item, itemIndex) => (
+                <AddColumnModalItem
+                  key={`add-column-button-group-${groupIndex}-item-${itemIndex}`}
+                  availableWidth={availableWidth}
+                  disabled={hasReachedColumnLimit}
+                  icon={item.icon}
+                  payload={item.payload}
+                  title={item.title}
+                />
+              ))}
+            </View>
+          </View>
+        ))}
+
+        {!!hasReachedColumnLimit && (
+          <SpringAnimatedText
+            style={{
+              marginTop: contentPadding,
+              lineHeight: 20,
+              fontSize: 14,
+              color: springAnimatedTheme.foregroundColorMuted50,
+              textAlign: 'center',
+            }}
+          >
+            {`You have reached the limit of ${
+              constants.COLUMNS_LIMIT
+            } columns. This is to maintain a healthy usage of the GitHub API.`}
+          </SpringAnimatedText>
+        )}
       </View>
     </ModalColumn>
   )

packages/components/src/components/cards/partials/EventCardHeader.tsx
@@ -190,7 +190,7 @@ export function EventCardHeader(props: EventCardHeaderProps) {
             size={18}
             style={{
               alignSelf: smallLeftColumn ? 'center' : 'flex-start',
-              marginTop: 6,
+              marginTop: 4,
               paddingVertical: 0,
               paddingHorizontal: contentPadding / 3,
             }}
@@ -211,7 +211,7 @@ export function EventCardHeader(props: EventCardHeaderProps) {
             size={18}
             style={{
               alignSelf: smallLeftColumn ? 'center' : 'flex-start',
-              marginTop: 6,
+              marginTop: 4,
               paddingVertical: 0,
               paddingHorizontal: contentPadding / 3,
               marginRight: -contentPadding / 2,

packages/components/src/components/cards/partials/NotificationCardHeader.tsx
@@ -227,7 +227,7 @@ export function NotificationCardHeader(props: NotificationCardHeaderProps) {
             size={18}
             style={{
               alignSelf: smallLeftColumn ? 'center' : 'flex-start',
-              marginTop: 6,
+              marginTop: 4,
               paddingVertical: 0,
               paddingHorizontal: contentPadding / 3,
             }}
@@ -248,7 +248,7 @@ export function NotificationCardHeader(props: NotificationCardHeaderProps) {
             size={18}
             style={{
               alignSelf: smallLeftColumn ? 'center' : 'flex-start',
-              marginTop: 6,
+              marginTop: 4,
               paddingVertical: 0,
               paddingHorizontal: contentPadding / 3,
               marginRight: -contentPadding / 2,

packages/components/src/components/columns/ColumnHeaderItem.tsx
@@ -149,13 +149,15 @@ export const ColumnHeaderItem = React.memo((props: ColumnHeaderItemProps) => {
 
   const styles = {
     container: {
-      alignSelf: 'flex-start',
+      alignSelf: 'center',
       justifyContent: 'center',
       alignItems: 'center',
       alignContent: 'center',
+      overflow: 'hidden',
     } as ViewStyle,
 
     mainContainer: {
+      flex: 1,
       flexDirection: 'row',
       alignItems: 'center',
       alignContent: 'center',
@@ -163,8 +165,6 @@ export const ColumnHeaderItem = React.memo((props: ColumnHeaderItemProps) => {
     } as ViewStyle,
 
     icon: {
-      height: size,
-      lineHeight: size,
       fontSize: size,
       textAlign: 'center',
     } as TextStyle,
@@ -172,7 +172,7 @@ export const ColumnHeaderItem = React.memo((props: ColumnHeaderItemProps) => {
     title: {
       fontWeight: '500',
       marginRight: contentPadding / 2,
-      lineHeight: size,
+      lineHeight: size - 1,
       fontSize: size - 3,
     } as TextStyle,
 
@@ -270,21 +270,22 @@ export const ColumnHeaderItem = React.memo((props: ColumnHeaderItemProps) => {
             </View>
           )}
 
-          <View style={{ flexDirection: 'row', maxWidth: 170 }}>
-            {!!title && (
-              <SpringAnimatedText
-                numberOfLines={1}
-                selectable={selectable}
-                style={[
-                  styles.title,
-                  { color: springAnimatedStyles.foregroundColor },
-                  titleStyle,
-                ]}
-              >
-                {title}
-              </SpringAnimatedText>
-            )}
-          </View>
+          {!!title && (
+            <SpringAnimatedText
+              numberOfLines={1}
+              selectable={selectable}
+              style={[
+                styles.title,
+                {
+                  flex: 1,
+                  color: springAnimatedStyles.foregroundColor,
+                },
+                titleStyle,
+              ]}
+            >
+              {title}
+            </SpringAnimatedText>
+          )}
 
           {!!subtitle && (
             <SpringAnimatedText

packages/components/src/components/columns/EventOrNotificationColumn.tsx
@@ -106,9 +106,11 @@ export const EventOrNotificationColumn = React.memo(
             iconName={requestTypeIconAndData.icon}
             subtitle={`${requestTypeIconAndData.subtitle || ''}`.toLowerCase()}
             title={`${requestTypeIconAndData.title || ''}`.toLowerCase()}
+            titleStyle={{ flex: 1 }}
+            style={{ flex: 1, alignItems: 'flex-start' }}
           />
 
-          <Spacer flex={1} />
+          <Spacer width={contentPadding / 2} />
 
           <ColumnHeaderItem
             analyticsLabel="clear_column"

packages/components/src/components/common/SubHeader.tsx
@@ -1,0 +1,52 @@
+import React from 'react'
+
+import { contentPadding } from '../../styles/variables'
+import { SpringAnimatedView } from '../animated/spring/SpringAnimatedView'
+import { getColumnHeaderThemeColors } from '../columns/ColumnHeader'
+import {
+  ColumnHeaderItem,
+  ColumnHeaderItemProps,
+} from '../columns/ColumnHeaderItem'
+import { useSpringAnimatedTheme } from '../context/SpringAnimatedThemeContext'
+import { useTheme } from '../context/ThemeContext'
+import { getSeparatorThemeColor } from './Separator'
+import { Spacer } from './Spacer'
+
+export interface SubHeaderProps extends ColumnHeaderItemProps {
+  children?: React.ReactNode
+}
+
+export function SubHeader(props: SubHeaderProps) {
+  const { children, ...restProps } = props
+
+  const springAnimatedTheme = useSpringAnimatedTheme()
+  const theme = useTheme()
+
+  return (
+    <SpringAnimatedView
+      style={{
+        alignSelf: 'stretch',
+        flexDirection: 'row',
+        paddingHorizontal: contentPadding / 2,
+        backgroundColor:
+          springAnimatedTheme[
+            getColumnHeaderThemeColors(theme.backgroundColor).normal
+          ],
+      }}
+    >
+      <ColumnHeaderItem
+        analyticsLabel={undefined}
+        fixedIconSize
+        size={18}
+        {...restProps}
+        style={[{ paddingVertical: contentPadding / 2 }, restProps.style]}
+        title={`${restProps.title || ''}`.toLowerCase()}
+        titleStyle={{ fontWeight: '400' }}
+      />
+
+      <Spacer flex={1} minWidth={contentPadding / 2} />
+
+      {children}
+    </SpringAnimatedView>
+  )
+}

packages/components/src/libs/github/index.ts
@@ -28,34 +28,39 @@ const cache: Record<
 > = {}
 
 export async function getNotifications(
-  _params: Octokit.ActivityListNotificationsParams & { headers?: any } = {},
+  params: (
+    | Octokit.ActivityListNotificationsParams
+    | Octokit.ActivityListNotificationsForRepoParams) & { headers?: any } = {},
   { subscriptionId = '', useCache = false } = {},
 ) {
-  const cacheKey = JSON.stringify(['NOTIFICATIONS', _params, subscriptionId])
+  const cacheKey = JSON.stringify(['NOTIFICATIONS', params, subscriptionId])
   const cacheValue = cache[cacheKey]
 
-  const params = _params || {}
-  params.headers = params.headers || {}
-  params.headers['If-None-Match'] = ''
+  const _params = params || {}
+  _params.headers = _params.headers || {}
+  _params.headers['If-None-Match'] = ''
 
   // Note: GitHub notifications cache doesnt work as expected.
   // It keeps returning code 304 even if read status changed.
   // Thats why its disabled by default.
   if (cacheValue && useCache) {
     if (cacheValue.headers['last-modified']) {
-      params.headers['If-Modified-Since'] = cacheValue.headers['last-modified']
+      _params.headers['If-Modified-Since'] = cacheValue.headers['last-modified']
     }
 
     if (cacheValue.headers.etag) {
-      params.headers['If-None-Match'] = cacheValue.headers.etag
+      _params.headers['If-None-Match'] = cacheValue.headers.etag
     }
   }
 
   // So lets force an update
-  if (!useCache) (params as any).timestamp = Date.now()
+  if (!useCache) (_params as any).timestamp = Date.now()
 
   try {
-    const response = await octokit.activity.listNotifications(params)
+    const response =
+      'owner' in _params && _params.owner && _params.repo
+        ? await octokit.activity.listNotificationsForRepo(_params)
+        : await octokit.activity.listNotifications(_params)
 
     cache[cacheKey] = {
       data: response.data,

commit 22ee2f6886527b0ce5f55cecbe82290ade49ab14

Fix wrong default filters for user activity column
Caused by: https://github.com/devhubapp/devhub/commit/65d6a73876b8238c698d06b4fef4736a7f5ccc61

Also move user dashboard default filters to AddColumnDetailsModal file

packages/components/src/components/modals/AddColumnDetailsModal.tsx
@@ -778,6 +778,15 @@ function getNewColumnAndSubscriptions(
             subtype,
           })
 
+          if (subtype === 'USER_RECEIVED_EVENTS') {
+            newColumnFilters.subjectTypes = newColumnFilters.subjectTypes || {
+              Release: true,
+              Repository: true,
+              Tag: true,
+              User: true,
+            }
+          }
+
           break
         }
 

packages/components/src/components/modals/AddColumnModal.tsx
@@ -121,14 +121,6 @@ const columnTypes: Array<{
             subtype: 'USER_EVENTS',
           },
           isPrivateSupported: false,
-          defaultFilters: {
-            subjectTypes: {
-              Release: true,
-              Repository: true,
-              Tag: true,
-              User: true,
-            },
-          },
         },
       },
       {